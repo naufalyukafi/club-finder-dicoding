a4c2ad6fe89476baba661ec5fe8f29d8868e8e12
Naufal Yukafi <naufalyukafi10@gmail.com>
2020-07-29 22:44:53 +0700

    add project latihan

---
+++ latihan/belajar-custom-element/main.js
@@ -0,0 +1,20 @@
+// if (!imageFigureElement) {
+//   imageFigureElement = document.createElement("image-figure");
+//   document.body.appendChild(imageFigureElement);
+// }
+
+// //mengubah/menambah nilai attribute caption
+// setTimeout(() => {
+//   imageFigureElement.setAttribute("caption", "Gambar 1");
+// }, 1000);
+
+// setTimeout(() => {
+//   imageFigureElement.remove();
+// }, 3000);
+// const imageFigureElement = document.createElement("image-figure");
+
+// imageFigureElement.setAttribute("src", "https://i.imgur.com/iJq78XH.jpg");
+// imageFigureElement.setAttribute("alt", "Dicoding Logo");
+// imageFigureElement.setAttribute("caption", "Huruf g dalam logo Dicoding");
+
+// document.body.appendChild(imageFigureElement);
